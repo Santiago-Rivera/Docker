9769e21a5b11c6707a76412818bd5d93e0543f71
Santiago Rivera <sarivera.es@tecnologicoargos.edu.ec>
2025-04-19 23:00:04 -0500

    Docker change

--- myapp.mjs
+++ myapp.mjs
@@ -3,7 +3,7 @@ import connecToDatabese from './helpers.mjs';
 
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');   
